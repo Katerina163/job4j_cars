ce01c6e5e836622bfe8e984176c35e62b601e155
Katerina <Nefedova.Katia@yandex.ru>
2023-05-15 18:03:27 +0400

    Конфигурация Hibernate

---
+++ src/main/java/ru/job4j/cars/repository/UserRepository.java
@@ -0,0 +1,136 @@
+package ru.job4j.cars.repository;
+
+import lombok.AllArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import ru.job4j.cars.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UserRepository {
+    private final SessionFactory sf;
+
+    /**
+     * Сохранить в базе.
+     *
+     * @param user пользователь.
+     * @return пользователь с id.
+     */
+    public User create(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        return user;
+    }
+
+    /**
+     * Обновить в базе пользователя.
+     *
+     * @param user пользователь.
+     */
+    public void update(User user) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "update User set login = :fLogin, password = :fPassword where id = :fId")
+                    .setParameter("fLogin", user.getLogin())
+                    .setParameter("fPassword", user.getPassword())
+                    .setParameter("fId", user.getId())
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+    }
+
+    /**
+     * Удалить пользователя по id.
+     *
+     * @param userId ID
+     */
+    public void delete(int userId) {
+        Session session = sf.openSession();
+        try {
+            session.beginTransaction();
+            session.createQuery(
+                            "delete User where id = :fId")
+                    .setParameter("fId", userId)
+                    .executeUpdate();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+    }
+
+    /**
+     * Список пользователь отсортированных по id.
+     *
+     * @return список пользователей.
+     */
+    public List<User> findAllOrderById() {
+        Session session = sf.openSession();
+        Query<User> query = session.createQuery(
+                "from User order by id", User.class);
+        List<User> list = query.list();
+        session.close();
+        return list;
+    }
+
+    /**
+     * Найти пользователя по ID
+     *
+     * @return пользователь.
+     */
+    public Optional<User> findById(int userId) {
+        Session session = sf.openSession();
+        Query<User> query = session.createQuery(
+                "from User where id = :fId", User.class)
+                .setParameter("fId", userId);
+        Optional<User> optionalUser = query.uniqueResultOptional();
+        session.close();
+        return optionalUser;
+    }
+
+    /**
+     * Список пользователей по login LIKE %key%
+     *
+     * @param key key
+     * @return список пользователей.
+     */
+    public List<User> findByLikeLogin(String key) {
+        Session session = sf.openSession();
+        Query<User> query = session.createQuery(
+                "from User where password = :fKey", User.class)
+            .setParameter("fKey", key);
+        List<User> list = query.list();
+        session.close();
+        return list;
+    }
+
+    /**
+     * Найти пользователя по login.
+     *
+     * @param login login.
+     * @return Optional or user.
+     */
+    public Optional<User> findByLogin(String login) {
+        Session session = sf.openSession();
+        Query<User> query = session.createQuery(
+                        "from User where login = :fLogin", User.class)
+                .setParameter("fLogin", login);
+        Optional<User> optionalUser = query.uniqueResultOptional();
+        session.close();
+        return optionalUser;
+    }
+}
