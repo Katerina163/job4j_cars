2ff40b8eefe00b327c6b282715c5579db925b719
Katerina <Nefedova.Katia@yandex.ru>
2023-05-23 11:42:11 +0400

    ManyToMany

---
+++ db/scripts/005_ddl_create_participates_table.sql
@@ -0,0 +1,7 @@
+create table participates
+(
+    id      serial primary key,
+    user_id int not null references auto_user(id),
+    post_id int not null references auto_post(id),
+            unique (user_id, post_id)
+);
--- src/main/java/ru/job4j/cars/model/PriceHistory.java
+++ src/main/java/ru/job4j/cars/model/PriceHistory.java
@@ -3,6 +3,7 @@ package ru.job4j.cars.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -20,4 +21,12 @@ public class PriceHistory {
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "auto_post_id")
     private List<AutoPost> autoPost;
+
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "post_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    private List<User> participates = new ArrayList<>();
 }
