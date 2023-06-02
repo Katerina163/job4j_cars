8f59f58c1785d7962dedbe9163840a221a75ca85
Katerina <Nefedova.Katia@yandex.ru>
2023-06-02 11:58:57 +0400

    добавление контроллера для css

---
+++ src/main/java/ru/job4j/cars/configuration/StylesConfiguration.java
@@ -0,0 +1,17 @@
+package ru.job4j.cars.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class StylesConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/styles/**")
+                .addResourceLocations("classpath:/static/styles/");
+    }
+}
