7a56471a683d24db1106b016a3fb7432a6e5141a
Katerina <Nefedova.Katia@yandex.ru>
2023-06-02 10:47:51 +0400

    конфигурация hibernate

---
+++ src/main/java/ru/job4j/cars/configuration/HibernateConfiguration.java
@@ -0,0 +1,24 @@
+package ru.job4j.cars.configuration;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.job4j.cars.repository.CrudRepository;
+
+@Configuration
+public class HibernateConfiguration {
+    @Bean(destroyMethod = "close")
+    public SessionFactory sf() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        return new MetadataSources(registry).buildMetadata().buildSessionFactory();
+    }
+
+    @Bean
+    public CrudRepository crud() {
+        return new CrudRepository(sf());
+    }
+}
