0085b794967ec923d297da1c6dde79fe7f79f44d
Katerina <Nefedova.Katia@yandex.ru>
2023-06-05 18:23:25 +0400

    корректировка

--- db/scripts/012_ddl_create_files_table.sql
+++ db/scripts/012_ddl_create_files_table.sql
@@ -3,5 +3,5 @@ create table if not exists files
     id   serial primary key,
     name varchar not null,
     path varchar not null unique,
-    post_id int not null references auto_post(id)
+    post_id int references auto_post(id)
 );
