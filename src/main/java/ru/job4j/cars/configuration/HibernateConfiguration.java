1bfec112a88358c44e139cd2d9de0556a7aa5618
Katerina <Nefedova.Katia@yandex.ru>
2023-06-26 21:51:21 +0400

    корректировка конфигурации

--- src/main/java/ru/job4j/cars/configuration/HibernateConfiguration.java
+++ src/main/java/ru/job4j/cars/configuration/HibernateConfiguration.java
@@ -6,7 +6,6 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.job4j.cars.repository.CrudRepository;
 
 @Configuration
 public class HibernateConfiguration {
@@ -15,10 +14,5 @@ public class HibernateConfiguration {
         final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
                 .configure().build();
         return new MetadataSources(registry).buildMetadata().buildSessionFactory();
-    }
-
-    @Bean
-    public CrudRepository crud() {
-        return new CrudRepository(sf());
     }
 }
